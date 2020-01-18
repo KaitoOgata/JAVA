e006907b847ece396ddee5bac6c8b803f6ed0f31
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-01-17 18:39:19 -0800

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,1 +1,17 @@
-huj
+public class Fibonacci {
+
+    public static int fib(int n) {
+        if (n <= 1) return n;
+        else return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[]args) {
+
+        for (int i = 0; i < 12; i++) {
+            System.out.println(fib(i));
+        }
+
+    }
+
+
+}
