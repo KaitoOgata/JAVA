15b7aa98f65bb79152e1414572bb028e3121f3f0
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-01-29 14:07:33 -0800

    Update printPowersOfN.java

--- printPowersOfN.java
+++ printPowersOfN.java
@@ -1,12 +1,16 @@
+/*ITC115 Kaito Ogata*/
 public class java1 {
     public static void main(String[] args) {
-    	System.out.println(printPowersOfN(2,7));
+    	/*Method to test first example*/
+        System.out.println(printPowersOfN(2,7));
+        /*Method to test second example*/
     	System.out.println(printPowersOfN(5,3));
 }
     public static int printPowersOfN (int b, int e)
     {
     	int number = 1;
     	for (int i=0; i < e; i++)
+           /*Calculate powers by using Math function*/
     	{System.out.print((int) Math.pow(b, i) + " ");
     		number*=b;
     	}
