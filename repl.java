e2e4bdd094e6cff6086a82a088c05dd419d85dcd
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-02-02 18:13:00 -0800

    Update repl.java

--- repl.java
+++ repl.java
@@ -6,3 +6,28 @@
 //By: 
 //Feb 2 2020
 //*******************************************************************
+import java.io.*;
+import java.util.Scanner;
+
+public class java1 {
+    public static void main(String[] args) {
+    	
+    	String str, output;
+    	
+    	Scanner input =new Scanner(System.in);
+    	output=repl("hello", 3);
+    	System.out.println(output);
+    }
+    public static String repl(String str,int times)
+    {
+    	String newStr="";
+    	if(times<=0)
+    		return newStr;
+    	else
+    	{
+    		for (int i=1;i<=times; i++ )
+    			newStr=newStr+str;
+    		return newStr;
+    	}
+    }
+}
