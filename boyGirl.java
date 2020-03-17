8e8a8c176ed2496f2b08381f851fee9ebf325f07
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-03-16 22:35:05 -0700

    Create boyGirl.java

---
+++ boyGirl.java
@@ -0,0 +1,28 @@
+ //*******************************************************************
+//By: Kaito Ogata
+//Date March 16 2020
+//*******************************************************************
+
+ public static void boyGirl(Scanner console) {
+    //decrare variable
+    int boy = 0;
+    int bSum = 0;
+    int girl = 0;
+    int gSum = 0;
+    int i = 2;
+    
+    while (console.hasNext()) {
+        console.next();
+        if ((i / 2) % 2 != 0) {
+            bSum += console.nextInt();
+            boy++;
+        } else {
+            gSum += console.nextInt();
+            girl++;
+        }
+        i += 2;
+    }
+    
+    System.out.println(boy + " boys, " + girl + " girls");
+    System.out.println("Difference between boys' and girls' sums: " + Math.abs(bSum - gSum));
+}
