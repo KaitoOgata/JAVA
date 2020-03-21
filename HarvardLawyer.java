67cb2c456e27dc6b8a50ff1abc56ca7a53dc0ee8
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-03-21 01:14:37 -0700

    Create HarvardLawyer.java

---
+++ HarvardLawyer.java
@@ -0,0 +1,15 @@
+//create class called Lawyer
+public class Lawyer extends Employee
+{
+    // overrides Emloyee class
+    public int getVacationDays()
+    {
+        return 15;
+    }
+    // overrides Emloyee class
+    public string getVacationForm()
+    {
+        return "pink";
+        }
+        
+    }
