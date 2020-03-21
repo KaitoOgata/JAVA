881ce72f440d7ea97bcea3e8da54a08595a292af
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-03-21 01:03:29 -0700

    Create Janitor.java

---
+++ Janitor.java
@@ -0,0 +1,21 @@
+public class Employee
+{
+
+public int getHours()
+{
+return 40;
+}
+public double getSalary()
+{
+return 40000.0;
+}
+   
+public int geVacationDays()
+{
+return 10;
+}
+public String getVacatonForm()
+{
+return "yellow";
+ }
+}
