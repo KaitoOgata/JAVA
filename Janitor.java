60b5d4652f7b659defd521c38021dd62499c42ed
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-03-21 01:16:23 -0700

    Update Janitor.java

--- Janitor.java
+++ Janitor.java
@@ -1,19 +1,22 @@
+// class for general Employee
 public class Employee
 {
-
+// return hours
 public int getHours()
 {
 return 40;
 }
+// return salary
 public double getSalary()
 {
 return 40000.0;
 }
-   
+// return vacation days   
 public int geVacationDays()
 {
 return 10;
 }
+// return string
 public String getVacatonForm()
 {
 return "yellow";
