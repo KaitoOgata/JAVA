f7d41fe4e97eeda89b81f2aca3f744128eeea316
KaitoOgata <57466932+KaitoOgata@users.noreply.github.com>
2020-03-23 14:29:03 -0700

    Create final.java

---
+++ final.java
@@ -0,0 +1,30 @@
+public class final {
+	//variables
+  private String _title;
+	private String _functionality;
+	private Number _daysLate;
+	private Number _fines;
+	//constructor
+	public final (String title,String functionality, Number daysLate, Number fines ) {
+		_title = title;
+		_functionality = functionality;
+		_daysLate = daysLate;
+		_fines = fines;
+	}
+	//getters
+	public String getTitle() { return _title; }
+	public String getFunctionality() { return _functionality; }
+	public Number getdaysLate() { return _daysLate; }
+	public Number getfines() { return _fines; }
+	//setters
+	public void setTitle(String title) {_title = title;}
+	public void setFunctionality(String functionality) {_functionality = functionality;}
+	public void setdaysLate(Number daysLate) {_daysLate = daysLate;}
+	public void setfines(Number fines) {_fines = fines;}
+	
+	public String toString()
+	{
+		return "Title: " + _title + "\n" + "Functionality: " + _functionality + "\n" + "daysLate:" + _daysLate +
+				"Fines:" + _fines + "\n\n";
+	}
+}
